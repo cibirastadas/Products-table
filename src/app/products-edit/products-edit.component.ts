8a34f7380f223da20e02672dedb0567f3fe8b159
cibirastadas <you@example.com>
2021-01-12 13:57:40 +0200

    Added validation

--- src/app/products-edit/products-edit.component.ts
+++ src/app/products-edit/products-edit.component.ts
@@ -1,67 +1,91 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormBuilder, FormArray } from "@angular/forms"
-import * as uuid from 'uuid';
+import { FormGroup, FormBuilder, FormArray, Validators } from "@angular/forms"
 import { ProductsService } from '../services/products.service';
 import { Product } from '../models/product.module';
+import { ChangeDetectorRef } from '@angular/core'
 @Component({
   selector: 'app-products-edit',
   templateUrl: './products-edit.component.html',
   styleUrls: ['./products-edit.component.css']
 })
 export class ProductsEditComponent implements OnInit {
-  myId = uuid.v4();
   productsForm : FormGroup;
   heading: string;
   mode: Boolean;
   id: Number;
-  products;
-  constructor(private router: Router, private activeRoute: ActivatedRoute,private fb: FormBuilder, private productService: ProductsService) { 
+  newProduct : Product;
+  products: Product;
+  constructor(private router: Router, private activeRoute: ActivatedRoute,private fb: FormBuilder, private productService: ProductsService, private readonly changeDetectorRef: ChangeDetectorRef) { 
   }
 
   ngOnInit(): void {
     this.productsForm = this.fb.group({
       id : 0,
-      name : [""],
+      name : ["",[
+        Validators.required
+      ]],
       creationDate: [""],
       categories : this.fb.array([this.fb.control("")]),
-      price : [0],
+      price : [0, [
+        Validators.required,
+        Validators.min(1)
+      ]],
       currency : "EUR"
     })
-    this.mode= this.router.url === "/add";
+    this.mode = this.router.url === "/add";
     this.heading = this.mode ? "Add Products" : "Edit Products"
-    this.id=+ this.activeRoute.snapshot.paramMap.get('id');
+    this.id =+ this.activeRoute.snapshot.paramMap.get('id');
     if(!this.mode){
         this.products = this.productService.getOneProduct(this.id)
         this.patchValues();
     }
-   /*  this.productsForm.valueChanges.subscribe(console.log) */
   }
 
   onSubmit(): void{
+    this.newProduct = this.productsForm.value
+    this.newProduct.creationDate = new Date().toString();
 
     if(this.mode){
-      let newProduct: Product = this.productsForm.value
-      newProduct.creationDate = new Date().toString();
-      newProduct.id = this.getRndInteger();
-      this.productService.onAdd(newProduct)
+
+      this.newProduct.id = this.getRndInteger();
+      this.productService.onAdd(this.newProduct)
     }else{
-      let newProduct: Product = this.productsForm.value
-      newProduct.id = this.products.id
-      newProduct.creationDate = this.products.creationDate
-      this.productService.onUpdate(newProduct)
+      this.productService.onUpdate(this.newProduct)
     }
     this.router.navigateByUrl("")
   }
   
-  addCategories(){
+  addCategories(): void{
+  this.ngAfterViewChecked()
     this.categories.push(this.fb.control(""))
   }
 
-  removeCategories(index){
+  removeCategories(index: number) : void{
     this.categories.removeAt(index)
   }
   
+  patchValues(): void{
+    this.products.categories.forEach(element => {
+      this.categories.push(this.fb.control(""))
+    });
+    this.productsForm.patchValue({
+      id : this.products.id,
+      name : this.products.name,
+      categories : this.products.categories,
+      price : this.products.price,
+    })
+    this.categories.removeAt(this.categories.length -1)
+  }
+
+  getRndInteger() {
+  return Math.floor(Math.random() * (99999999 - 10000000  + 1) ) + 10000000;
+  }
+
+  ngAfterViewChecked(): void {
+    this.changeDetectorRef.detectChanges();
+  }
+
   get categories() {
     return this.productsForm.get('categories') as FormArray;
   }
@@ -73,20 +97,4 @@ export class ProductsEditComponent implements OnInit {
   get price(){
     return this.productsForm.get('price');
   }
-
-  patchValues() {
-    this.products.categories.forEach((a) => { 
-       this.categories.push(this.patch(a.categories)) 
-    });
-    this.categories.removeAt(this.categories.length -1)
-  }
-
-  patch(categorie) {
-    return this.fb.control(categorie);
-  }
-
-  getRndInteger() {
-  return Math.floor(Math.random() * (99999999 - 10000000  + 1) ) + 10000000;
-  }
-
 }
